08227d06d26cbbb89f03124cc4bee4d9e416f837
babysean <tkdgns7070@gmail.com>
2022-07-22 15:23:59 +0900

    feat: Static file VS Dynamic file
    static file : static.html
    dynamic file : /dynamic
    
    static file은 node를 재시작하지 않아도 변경사항이 반영
    danamic file은 node를 재시작해야 변경사항이 반영

--- app.js
+++ app.js
@@ -11,6 +11,34 @@ app.get('/', (req, res) => {
     res.send('Hello home page');
 });
 
+// 사용자가 /dynamic으로 접속했을때 실행될 함수, 동적파일
+app.get('/dynamic', (req, res) => {
+    let lis = ''
+    let time = Date()
+
+    for(let i=0 ; i<5 ; i++){
+        lis += '<li>coding</li>'
+    }
+
+    let output = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Static HTML</title>
+        </head>
+        <body>
+            Hello, Dynamic!
+            <ul>
+                ${lis}
+            </ul>
+            ${time}
+        </body>
+        </html>`
+
+    res.send(output)
+})
+
 // 사용자가 /route로 접속했을 때 실행될 함수
 // public 이라는 정적 파일 디렉토리를 선언하였기 때문에 /route.png 형식으로 접근 가능
 app.get('/route', (req, res) => {
@@ -19,7 +47,7 @@ app.get('/route', (req, res) => {
 
 // 사용자가 /login 으로 접속했을 때 실행될 함수
 app.get('/login', (req, res) => {
-    res.send('<h1>Login please</h1>');
+    res.send('<h1>Login please</h1>')
 })
 
 // 서비스 포트 설정, 3000
