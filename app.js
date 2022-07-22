0db3a10ccd795ed2c97714717496d898859afc56
babysean <tkdgns7070@gmail.com>
2022-07-22 15:04:00 +0900

    feat: Static file controll
    1. Static file 디렉토리 설정 방법
    2. express.get을 이용한 static file 접근 방법

--- app.js
+++ app.js
@@ -3,10 +3,19 @@
 var express = require('express')
 var app = express()
 
+// public 이라는 디렉토리에 있는 정적인 파일 제공
+app.use(express.static('public'));
+
 // 사용자가 /로 접속했을 때 실행될 함수
 app.get('/', (req, res) => {
     res.send('Hello home page');
 });
+
+// 사용자가 /route로 접속했을 때 실행될 함수
+// public 이라는 정적 파일 디렉토리를 선언하였기 때문에 /route.png 형식으로 접근 가능
+app.get('/route', (req, res) => {
+    res.send('Hello Router, <img src="/route.png"/>')
+})
 
 // 사용자가 /login 으로 접속했을 때 실행될 함수
 app.get('/login', (req, res) => {
